972c1dade94c875d4597cacb6e1e0674e0739e08
CoderIrfann <irfankhan9888477@gmail.com>
2024-06-30 17:27:59 +0530

    Reverse Technique in java DSA

--- reverseOfArray.java
+++ reverseOfArray.java
@@ -2,26 +2,25 @@
  * reverseOfArray
  */
 public class reverseOfArray {
-    public static void reverse(int number[]){
+    public static void reverse(int number[]) {
         int first = 0;
-        int last = number.length-1;
-        while (first<last) {
+        int last = number.length - 1;
+        while (first < last) {
             int temp = number[first];
-            number[first] =  number[last];
+            number[first] = number[last];
             number[last] = temp;
             first++;
             last--;
 
-            
         }
     }
 
     public static void main(String[] args) {
-     int number[] = {10, 9 ,8 ,7  , 6, 4,3};
-     reverse(number); 
-     
-     for(int i = 0; i< number.length; i++){
-        System.out.println(number[i]);
-    }  
+        int number[] = { 10, 9, 8, 7, 6, 4, 3 };
+        reverse(number);
+
+        for (int i = 0; i < number.length; i++) {
+            System.out.print(number[i] + " ");
+        }
     }
 }
