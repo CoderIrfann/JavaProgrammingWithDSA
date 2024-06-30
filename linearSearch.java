8979f2a57eaacde53ac7019aa35ca9757a3acd42
CoderIrfann <irfankhan9888477@gmail.com>
2024-06-30 09:14:35 +0530

    Add a new Menthod linear method

--- linearSearch.java
+++ linearSearch.java
@@ -1,24 +1,45 @@
 public class linearSearch {
-    public static int linearSearchInArray(int number[] , int key){
+    // public static int linearSearchInArray(int number[] , int key){
+    //     for(int  i =0;i<number.length; i++){
+    //         if(number[i] == key){
+    //             return i;
+    //         }
+    //     }
+    //     return -1;
+    // }
+
+    // public static void main(String[] args) {
+
+    //     int number[] = {1 , 2 , 4 , 5 , 4 , 6 , 7};
+    //     int key = 4;
+
+    //     int index  = linearSearchInArray(number, key);
+
+    //     if(index == -1){
+    //         System.out.println("Something wents wrong");
+    //     }else{
+    //         System.out.println("Element is founded at index is : "+ index);
+    //     }
+    // }
+
+    // my method
+
+    public static void linearSearchInArray(int number[] , int key){
         for(int  i =0;i<number.length; i++){
             if(number[i] == key){
-                return i;
+               System.out.println("find the element successfully" +i);
             }
         }
-        return -1;
+       
     }
 
     public static void main(String[] args) {
 
         int number[] = {1 , 2 , 4 , 5 , 4 , 6 , 7};
+
         int key = 4;
-
-        int index  = linearSearchInArray(number, key);
-
-        if(index == -1){
-            System.out.println("Something wents wrong");
-        }else{
-            System.out.println("Element is founded at index is : "+ index);
-        }
+       linearSearchInArray(number, key);
     }
+
+
 }
