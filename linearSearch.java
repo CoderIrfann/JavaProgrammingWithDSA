f5de7eaaa5d7676ac7945f14ccd8915f6c76eb5a
CoderIrfann <irfankhan9888477@gmail.com>
2024-06-30 09:17:41 +0530

    Updating Linera Serch In java Code

--- linearSearch.java
+++ linearSearch.java
@@ -22,24 +22,24 @@ public class linearSearch {
     //     }
     // }
 
-    // my method
+    // my method - wrong method
 
-    public static void linearSearchInArray(int number[] , int key){
-        for(int  i =0;i<number.length; i++){
-            if(number[i] == key){
-               System.out.println("find the element successfully" +i);
-            }
-        }
+    // public static void linearSearchInArray(int number[] , int key){
+    //     for(int  i =0;i<number.length; i++){
+    //         if(number[i] == key){
+    //            System.out.println("find the element successfully" +i);
+    //         }
+    //     }
        
-    }
+    // }
 
-    public static void main(String[] args) {
+    // public static void main(String[] args) {
 
-        int number[] = {1 , 2 , 4 , 5 , 4 , 6 , 7};
+    //     int number[] = {1 , 2 , 4 , 5 , 4 , 6 , 7};
 
-        int key = 4;
-       linearSearchInArray(number, key);
-    }
+    //     int key = 4;
+    //    linearSearchInArray(number, key);
+    // }
 
 
 }
