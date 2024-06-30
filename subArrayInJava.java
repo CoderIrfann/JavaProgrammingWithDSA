d493af0733cd26dcc9acca3f4b16e437a128ded4
CoderIrfann <irfankhan9888477@gmail.com>
2024-07-01 00:19:15 +0530

    Updating a SubArrayinJava

--- subArrayInJava.java
+++ subArrayInJava.java
@@ -1,25 +1,23 @@
 public class subArrayInJava {
     public static void subArray(int number[]) {
+        int sum = 0;
+        int start;
+        int end;
         for (int i = 0; i < number.length; i++) {
-            int start = i;
+            start = i;
             for (int j = i; j < number.length; j++) {
-                int end = j;
-                for (int k = start; k < end; k++) {
-                    System.out.print(number[k] + " ");
+                end = j;
+                for (int k = start; k <= end; k++) {
+                    System.out.print(number[k]+ " ");
                 }
-            System.out.println();
-
+                sum = sum + j;
+                System.out.println();
             }
             System.out.println();
-
-
         }
-
-
-    }
-
+    } 
     public static void main(String[] args) {
-        int number[] = { 1, 3, 4, 5, 6 };
+        int number[] = {1,2,3,4,5};
         subArray(number);
     }
 
