4460833d643bf4108724b3507309417cb55497ff
CoderIrfann <irfankhan9888477@gmail.com>
2024-06-30 16:12:51 +0530

    Searching number using Binary Key

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,23 +1,28 @@
-/**
- * BinarySearch
- */
 public class BinarySearch {
-
-    public static  int BinarySearchInArray(int number[],int key){
-        int start= 0;
-        int end = number.length-1;
+    public static int binaryTree(int number[] , int key){
+        int start = 0;
+        int end = number.length;
         while (start <= end) {
         int mid = (start+end)/2;
+            if(number[mid] == key){ // when mid == key 
+                return mid;
+            }
+            if(number[mid] < key){ // when mid < key
+                start  = mid+1;
+            }else{
+                end = mid-1; // mid > key
+            }   
+        }
+        return -1;
 
-        if(number[mid] ==key ){
-            return mid;
-        }
-        if(number[mid] < key){//left side
-            return mid+1;
-        }
 
-            
-        }
+    }
+    public static void main(String[] args) {
+        int number[] = {1 ,2 ,3 ,4 ,5 ,6, 7};
+        int key = 2;
+        System.out.println("The Index of the number is : "+binaryTree(number, key));
         
     }
+
+    
 }
